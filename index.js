872fb3189ad08d4cb0978d9387ee3f1c5a4f5417
Kevin <kaiwei.lin@gmail.com>
2015-09-25 10:41:06 +0800

    create router

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-var http = require('http');
+var http = require("http");
+var url = require('url');
+var fs = require('fs');
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function(request, response) {
   console.log('Connection');
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write('Hello, World.');
-  response.end();
+  var path = url.parse(request.url).pathname;
+
+  switch (path) {
+    case '/':
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write('Hello, World.');
+      response.end();
+      break;
+    case '/socket.html':
+      fs.readFile(__dirname + path, function(error, data) {
+        if (error){
+          response.writeHead(404);
+          response.write("opps this doesn't exist - 404");
+        } else {
+          response.writeHead(200, {"Content-Type": "text/html"});
+          response.write(data, "utf8");
+        }
+        response.end();
+      });
+      break;
+    default:
+      response.writeHead(404);
+      response.write("opps this doesn't exist - 404");
+      response.end();
+      break;
+  }
 });
 
 server.listen(8001);
